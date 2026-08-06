47aac2b41f70c53403387446e0f478dc462af05e
Laraib <Labbuxt@gmail.com>
2026-08-06 22:19:44 +0530

    feat: updated reuirements

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
-
 from pathlib import Path
 
 import pandas as pd
 import streamlit as st
 
 from src.model_pipeline import load_dataset, predict_record, train_pipeline
+
+
+
 
 ROOT_DIR = Path(__file__).resolve().parent
 DATA_PATH = ROOT_DIR / 'data' / 'raw_data' / 'parkinsons.csv'
