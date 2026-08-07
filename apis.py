ca1797967a3ffabe84fa158febc1eee84233880c
Laraib <Labbuxt@gmail.com>
2026-08-07 23:30:14 +0530

    feat: working with apis

--- apis.py
+++ apis.py
@@ -4,7 +4,7 @@ app=FastAPI()
 
 def load_data():
     with open('./data/raw_data/pd.json','r') as f:
-        data=json.load(f)
+        data=json.load(f) 
     return data
 
 @app.get("/")
@@ -17,5 +17,13 @@ def about():
 
 @app.get("/data")
 def get_data():
-    data=load_data()
+    data=load_data() 
     return data
+
+@app.get("/patients/{patient_id}")
+def get_patient_data(patient_id: str=Path(...,description="Id of the patient in DB",example="phon_R01_S01_1")):
+    data=load_data()
+    for patient in data:
+        if patient["name"] == patient_id:
+            return patient["Jitter:DDP"]
+    return({"error": "Patient not found"})
