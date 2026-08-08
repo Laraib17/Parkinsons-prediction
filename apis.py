e0a05e47d5d1229048768fc712968f14c7d2cdd1
Laraib <Labbuxt@gmail.com>
2026-08-08 23:52:29 +0530

    feat: working with apis

--- apis.py
+++ apis.py
@@ -36,4 +36,4 @@ def sort_Data(sort_by:str=Query(...,description="Sort on basis of jitter"),order
     if order not in ['asc','desc']:
         raise HttpException(status_code=400, detail="Invalid order. Valid orders are: 'asc' or 'desc'")
     data=load_data()
-    sorted_data=sorted(data)
+    sorted_data=sorted(data,key=lambda x:x[sort_by],reverse=(order=='desc'))
