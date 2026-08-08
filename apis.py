5c206ea164c973916c105e65eb100e98a44db151
Laraib <Labbuxt@gmail.com>
2026-08-08 23:38:55 +0530

    feat: working with apis

--- apis.py
+++ apis.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Query
 import json
 app=FastAPI()
 
@@ -21,9 +21,18 @@ def get_data():
     return data
 
 @app.get("/patients/{patient_id}")
-def get_patient_data(patient_id: str=Path(...,description="Id of the patient in DB",example="phon_R01_S01_1")):
+def get_patient_data(patient_id: str):
     data=load_data()
     for patient in data:
         if patient["name"] == patient_id:
-            return patient["Jitter:DDP"]
-    return({"error": "Patient not found"})
+            return patient[patient_id]
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("./sort")
+def sort_Data(sort_by:str=Query(...,description="Sort on basis of jitter"),order:str=Query('asc',description="Order of sorting is asc or desc")):
+    valid_fields=["jitter","shimmer","NHR","HNR"]
+    if sort_by not in valid_fields:
+        raise HttpException(status_code=400, detail=f"Invalid sort field. Valid fields are: {', '.join(valid_fields)}")
+    if order not in ['asc','desc']:
+        raise HttpException(status_code=400, detail="Invalid order. Valid orders are: 'asc' or 'desc'")
+    data=load_data()
