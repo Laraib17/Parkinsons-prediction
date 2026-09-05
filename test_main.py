64f67a6a4a7cff266255ddddf6b69a59c2109d3c
Laraib <Labbuxt@gmail.com>
2026-09-05 16:46:45 +0530

    feat:Trying tests

---
+++ test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app import app
+client=TestClient(app)
+def test_read_root():
+    res=client.get("/")
+    assert res.status_code==200
+    assert "message" in res.json() or res.json is not None
+
+def test_prediction_ednpoint():
+    payload={
+        "feature1":0.5,
+        "feature1":1.5
+    }
+    res=client.post("/predict",json=payload)
