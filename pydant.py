2de505de5e8346f73c20845c6d5139ad6cd6f476
Laraib <Labbuxt@gmail.com>
2026-09-06 23:57:49 +0530

    feat:added paatients data test

--- pydant.py
+++ pydant.py
@@ -1,12 +1,18 @@
 from pydantic import StrictInt
 from pydantic import BaseModel
-class new(BaseModel):
+class newi(BaseModel):
     name:str
     age:StrictInt
-dicto={'name':"Laraib",'age':"30"}
-p1=new(**dicto)
+dicto={'name':"Laraib",'age':30}
+p1=newi(**dicto)
 def insert_patient_data():
     print(p1.age)
     print(p1.name)
     print("inserted data")
-insert_patient_data(newboe)
+insert_patient_data()
+def helper2(p: newi):
+    print(p.name)
+    print(p.age)
+dicto2={'name':'akkad','age':44}
+p2=newi(**dicto2)
+helper2(p2)
