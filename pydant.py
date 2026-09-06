6e1dc68946c0194a63452ea86638bbb50fe10da3
Laraib <Labbuxt@gmail.com>
2026-09-06 23:58:10 +0530

    feat:added paatients data test

--- pydant.py
+++ pydant.py
@@ -1,9 +1,13 @@
+from operator import contains
 from pydantic import StrictInt
 from pydantic import BaseModel
+from typing import List,Dict
 class newi(BaseModel):
     name:str
     age:StrictInt
-dicto={'name':"Laraib",'age':30}
+    allergy:List[str]
+    contact_details:Dict[str:str]
+dicto={'name':"Laraib",'age':30,'allergy':['pollen','dust'],'contacts':'phon_no':'','']}
 p1=newi(**dicto)
 def insert_patient_data():
     print(p1.age)
